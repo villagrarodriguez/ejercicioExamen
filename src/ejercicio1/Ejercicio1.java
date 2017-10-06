bce83902976fc35b7d5d3e87ca9c5e89b6db2248
alumno <alumno@LIMSFLAB04-15D.system-uap.adm>
2017-10-05 22:08:36 -0500

    firsth

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -5,6 +5,8 @@
  */
 package ejercicio1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -15,18 +17,67 @@ public class Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int numeroInicial = 1; //el numero donde empieza
-        int numeroFinal = 100; //el numero donde termina
-        for (int i = 1; i <= (numeroFinal - numeroInicial + 1); i++ ) { 
-            int divisores = 0; //un numero primo solo puede tener 2 divisores
-            int numeroActual = 0;
-            for (int e = 1; e <= i; e++) { //este for por ejemplo revisa 5 y lo divide por 1, luego 2, 3, 4 y hasta 5
-                numeroActual = e;
-                if ( (i % e) == 0) divisores++; //aumenta los divisores si se
-                /*Explicación*/if (divisores == 2)  {break;} 
-            }
-            if (numeroActual == i && i != 1)  System.out.println (i); //muestre el número si cumplió los requisitos, excepto
+        Scanner input = new Scanner(System.in);
         
-    }
+      
+        System.out.println("Examen PArcial");
+        System.out.println("");
+        System.out.println("Villagra Rodriguez Edgardo");
+        System.out.println("");
+        System.out.println("Telefonos Moviles");
+        System.out.println("");
+        System.out.println("(1) Samsung");
+        System.out.println("(2) iphone");
+        System.out.println("(3) Huawey");
+        System.out.println("");
+        System.out.println("Seleccione un modelo");
+        
+        char opcion = input.next().charAt(0);
+              
+        switch (opcion) {
+            case '1':   System.out.println(""); 
+                        System.out.println("Pantalla   :   8.4"); 
+                        System.out.println("Camara     :   8MP"); 
+                        System.out.println("Procesador :   1.4Ghz"); break;
+                
+            case '2':   System.out.println(""); 
+                        System.out.println("Pantalla   :   5.8"); 
+                        System.out.println("Camara     :   326ppp"); 
+                        System.out.println("Procesador :   Apple A11 Bionic "); break;
+                        
+            case '3':   System.out.println(""); 
+                        System.out.println("Pantalla   :   5.1"); 
+                        System.out.println("Camara     :   20MP"); 
+                        System.out.println("Procesador :   2.36 GHz. "); break;
+                
+                default:
+                    System.out.println("Seleccion incorrecta numeros entre 1 y 3");
+        
+                
+                    char operador = input.next().charAt(0);
+              
+        switch (operador) {
+            case 'c':   System.out.println(""); 
+                        System.out.println("Claro"); 
+                        System.out.println("plan        :  Control S/100 "); 
+                        System.out.println("Internet    :   Ilimitado");
+                        System.out.println("Minutos     :  500"); break;
+                
+            case 'm':   System.out.println(""); 
+                        System.out.println("Movistar"); 
+                        System.out.println("plan     :   326ppp"); 
+                        System.out.println("Procesador :   Apple A11 Bionic "); break;
+                        
+            case 'e':   System.out.println(""); 
+                        System.out.println("Pantalla   :   5.1"); 
+                        System.out.println("Camara     :   20MP"); 
+                        System.out.println("Procesador :   2.36 GHz. "); break;
+                
+                default:
+                    System.out.println("Seleccion incorrecta numeros entre 1 y 3");
+        }
+        
+        }
+    
     }   
 }
---
+++ src/ejercicio1/TelefonoMovil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class TelefonoMovil {
+    Scanner input = new Scanner (System.in);
+    
+    TelefonoMovil telefonoMovil01 = new TelefonoMovil();
+    telefonoMovil01.setPantalla("4.7");
+    TelefonoMovil01.setCamara("8MP");
+    
+    
+    
+    
+}
